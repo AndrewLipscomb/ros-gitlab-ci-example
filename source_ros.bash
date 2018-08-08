14eb25a04da9ca7b5619992337f8cc4930daa239
Andrew Lipscomb <andrew.lipscomb@swarmfarm.com>
2018-08-09 07:39:09 +1000

    Sources again

--- source_ros.bash
+++ source_ros.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -d "/opt/ros/melodic" ]; then
-    echo "Sourcing ROS Melodic";
-    source /opt/ros/melodic/setup.bash;
+    echo "Found ROS Melodic";
+    export ROS_SOURCE_BASH_FILE="/opt/ros/melodic/setup.bash";
 elif [ -d "/opt/ros/kinetic" ]; then
-    echo "Sourcing ROS Kinetic";
-    source /opt/ros/kinetic/setup.bash;
+    echo "Found ROS Kinetic";
+    export ROS_SOURCE_BASH_FILE="/opt/ros/kinetic/setup.bash";
 else 
     echo "Neither Melodic or Kinetic appear to be installed. Cannot source ROS dependencies";
     exit 1;
